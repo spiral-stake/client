d4846e25df8dc5da068f38318491cc2dec821692
Bhimgouda D Patil <satvikp508@gmail.com>
2025-03-31 01:08:56 +0530

    added build

--- dist/assets/index-B3cqRytg.js
+++ dist/assets/index-B3cqRytg.js
@@ -1,4 +1,4 @@
-import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-DXFAQDI7.js";import{q as Wr}from"./index-CDD14FdZ.js";/**
+import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-MaW4Crbo.js";import{q as Wr}from"./index-QhDzXAax.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
